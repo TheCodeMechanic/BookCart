cc90ae0d1d400dfcf095d16e2b1310f2d9e5ee9a
Rohit Sonar <rohitsonarpersonal@gmail.com>
2026-03-30 17:28:33 +0530

    second commmit

--- BookCart/src/java/BookCart/Data/BooksDB.java
+++ BookCart/src/java/BookCart/Data/BooksDB.java
@@ -27,8 +27,8 @@ public class BooksDB {
         Connection connection = pool.getConnection();
         PreparedStatement ps = null;
         List<BookReview> reviews = new ArrayList<BookReview>();
-        // sample commengt for demo
-        
+        // sample comment for demo
+
         
         ResultSet rs = null;
         
